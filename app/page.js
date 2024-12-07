4e561b5cad2338555353f4b61d43494e1797569e
peterhermansen <hermansenph@gmail.com>
2024-12-06 21:30:02 -0500

    Implemented Summary components on overview page with associated styling

--- app/page.js
+++ app/page.js
@@ -1,5 +1,30 @@
-import "../styles/globals.css";
+import fs from 'fs';
+import path from 'path';
+import '../styles/globals.css';
+import Summary from '@/components/overview/Summary';
 
-export default function Home() {
-  return <>Test</>;
+export default async function Page() {
+  // Read, parse, and assign JSON data to variable
+  const filePath = path.join(process.cwd(), 'data.json');
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  return (
+    <>
+      <h1 className="title">Overview</h1>
+      <div className="summaries">
+        <Summary
+          header="Current Balance"
+          value={data.balance.current}
+          style="dark"
+        />
+        <Summary header="Income" value={data.balance.income} style="light" />
+        <Summary
+          header="Expenses"
+          value={data.balance.expenses}
+          style="light"
+        />
+      </div>
+    </>
+  );
 }
