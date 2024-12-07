a4aa12df753c3b53a0fa3228be28680718bba132
peterhermansen <hermansenph@gmail.com>
2024-12-07 01:28:04 -0500

    Created PotsSummary component and associated styling

--- components/overview/Summary.js
+++ components/overview/Summary.js
@@ -1,6 +1,6 @@
 import styles from '@/styles/components/overview/Summary.module.css';
 
-const OverviewSummary = ({ header, value, style }) => {
+const Summary = ({ header, value, style }) => {
   // Keep 2 decimal places and add commas
   let formattedValue = value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
@@ -8,10 +8,16 @@ const OverviewSummary = ({ header, value, style }) => {
     <div
       className={`${styles.summary} ${style === 'dark' ? styles.dark : styles.light}`}
     >
-      <h4 className={styles['summary-title']}>{header}</h4>
-      <h1>${formattedValue}</h1>
+      <h4
+        className={`${styles['summary-title']} ${style === 'dark' ? styles['value-light'] : null}`}
+      >
+        {header}
+      </h4>
+      <h1 className={`${style === 'dark' ? styles['value-light'] : null}`}>
+        ${formattedValue}
+      </h1>
     </div>
   );
 };
 
-export default OverviewSummary;
+export default Summary;
