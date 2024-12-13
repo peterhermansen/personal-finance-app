405c5f4d193a9b5352310f338c9d60832b1d4563
peterhermansen <hermansenph@gmail.com>
2024-12-12 19:19:58 -0500

    Updated HeaderOverview component to have hover effect on button

--- components/overview/HeaderOverview.js
+++ components/overview/HeaderOverview.js
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import styles from '@/styles/components/overview/HeaderOverview.module.css';
 
 const HeaderOverview = ({ title, btnText }) => {
   return (
-    <div className="overview-section-header">
+    <div className={styles.header}>
       <h2 className="text-2 bold">{title}</h2>
-      <button className="btn--overview">
+      <button className={styles.btn}>
         <span className="text-4">{btnText}</span>
         <Image
           src="/images/icon-caret-right.svg"
